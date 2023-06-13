74a17ad6451650b1547089e009ae91bfced225eb
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-06-13 17:51:46 +0400

    updated with add and push instruction

--- EVM/vm.go
+++ EVM/vm.go
@@ -1,13 +1,35 @@
 package evm
 
-import "fmt"
-
 type Instruction byte
 
 const (
 	InstrPush Instruction = 0x0a //10
 	InstrAdd  Instruction = 0x0b //1
 )
+
+type Stack struct {
+	data []any
+	sp   int
+}
+
+func NewStack(size int) *Stack {
+	return &Stack{
+		data: make([]any, size),
+		sp:   0,
+	}
+
+}
+
+func (s *Stack) Push(v any) {
+	s.data[s.sp] = v
+	s.sp++
+}
+
+func (s *Stack) Pop() any {
+	value := s.data[0]
+	s.data = append(s.data[:0], s.data[1:]...)
+	return value
+}
 
 type VM struct {
 	data  []byte
@@ -28,7 +50,7 @@ func NewVM(data []byte) *VM {
 func (vm *VM) Run() error {
 	for {
 		instr := Instruction(vm.data[vm.ip])
-		fmt.Println(instr)
+		//fmt.Println(instr)
 		if err := vm.Exec(instr); err != nil {
 			return err
 		}
@@ -43,7 +65,7 @@ func (vm *VM) Run() error {
 func (vm *VM) Exec(instr Instruction) error {
 	switch instr {
 	case InstrPush:
-		vm.pushStack(vm.data[vm.ip] - 1)
+		vm.pushStack(vm.data[vm.ip-1])
 	case InstrAdd:
 		a := vm.stack[0]
 		b := vm.stack[1]
--- EVM/vm_test.go
+++ EVM/vm_test.go
@@ -7,10 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestStack(t *assert.TestingT) {
+	s := NewStack(128)
+	s.Push(1)
+	s.Push(2)
+
+	value := s.Pop()
+	assert.Equal(*t, value, 1)
+	fmt.Println(s.data)
+}
+
 func TestVM(t *testing.T) {
+
 	data := []byte{0x01, 0x0a, 0x02, 0x0a, 0x0b}
 	vm := NewVM(data)
 	assert.Nil(t, vm.Run())
-	vm.Run()
-	fmt.Printf("%+v", vm.stack)
+	assert.Equal(t, byte(3), vm.stack[vm.sp])
+
 }
