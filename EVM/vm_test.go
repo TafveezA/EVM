998e5e183cfe7e9ad13ba0e5050766b28adc4e33
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-06-13 16:50:25 +0400

    update ignored files

--- EVM/vm_test.go
+++ EVM/vm_test.go
@@ -3,11 +3,14 @@ package evm
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestVM(t *testing.T) {
 	data := []byte{0x01, 0x0a, 0x02, 0x0a, 0x0b}
 	vm := NewVM(data)
+	assert.Nil(t, vm.Run())
 	vm.Run()
 	fmt.Printf("%+v", vm.stack)
 }
