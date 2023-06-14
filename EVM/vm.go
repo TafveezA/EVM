a2e817facfccddb0e2f6281c50645cda6957c6f5
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-06-14 10:15:33 +0400

    updated with  push and pop functions

--- EVM/vm.go
+++ EVM/vm.go
@@ -3,8 +3,10 @@ package evm
 type Instruction byte
 
 const (
-	InstrPush Instruction = 0x0a //10
-	InstrAdd  Instruction = 0x0b //1
+	InstrPushInt  Instruction = 0x0a //10
+	InstrAdd      Instruction = 0x0b //1
+	InstrPushByte Instruction = 0x0c //
+	IntrPack      Instruction = 0x0d //
 )
 
 type Stack struct {
@@ -28,22 +30,21 @@ func (s *Stack) Push(v any) {
 func (s *Stack) Pop() any {
 	value := s.data[0]
 	s.data = append(s.data[:0], s.data[1:]...)
+	s.sp--
 	return value
 }
 
 type VM struct {
 	data  []byte
 	ip    int //instruction pointer
-	stack []byte
-	sp    int //stack pointer
+	stack *Stack
 }
 
 func NewVM(data []byte) *VM {
 	return &VM{
 		data:  data,
 		ip:    0,
-		stack: make([]byte, 1024),
-		sp:    -1,
+		stack: NewStack(128),
 	}
 }
 
@@ -64,20 +65,18 @@ func (vm *VM) Run() error {
 
 func (vm *VM) Exec(instr Instruction) error {
 	switch instr {
-	case InstrPush:
-		vm.pushStack(vm.data[vm.ip-1])
+	case InstrPushByte:
+		vm.stack.Push(byte(vm.data[vm.ip-1]))
+	case InstrPushInt:
+		vm.stack.Push(int(vm.data[vm.ip-1]))
 	case InstrAdd:
-		a := vm.stack[0]
-		b := vm.stack[1]
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
 		c := a + b
-		vm.pushStack(c)
+		vm.stack.Push(c)
+	case InstrPack:
+		n := vm.stack.Pop().(int)
 
 	}
 	return nil
-}
-
-func (vm *VM) pushStack(b byte) {
-	vm.sp++
-	vm.stack[vm.sp] = b
-
 }
