2d611b939dc06c856a3f27a9fbfa781edec09a31
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-06-14 10:44:28 +0400

    updated with vm_test.go file

--- EVM/vm_test.go
+++ EVM/vm_test.go
@@ -1,27 +1,29 @@
 package evm
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStack(t *assert.TestingT) {
+func TestStack(t *testing.T) {
 	s := NewStack(128)
 	s.Push(1)
 	s.Push(2)
 
 	value := s.Pop()
-	assert.Equal(*t, value, 1)
-	fmt.Println(s.data)
+	assert.Equal(t, value, 1)
+
+	value = s.Pop()
+	assert.Equal(t, value, 2)
 }
 
 func TestVM(t *testing.T) {
 
-	data := []byte{0x01, 0x0a, 0x02, 0x0a, 0x0b}
+	data := []byte{0x02, 0x0a, 0x61, 0x0c, 0x61, 0x0c, 0x0d}
 	vm := NewVM(data)
 	assert.Nil(t, vm.Run())
-	assert.Equal(t, byte(3), vm.stack[vm.sp])
+	result := vm.stack.Pop()
+	assert.Equal(t, 4, result)
 
 }
