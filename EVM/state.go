5b9e137a1d2f56fcc5461e46778609800ac24a5f
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-06-15 17:20:54 +0400

    modified main.go

--- EVM/state.go
+++ EVM/state.go
@@ -1,6 +1,8 @@
 package evm
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type State struct {
 	data map[string][]byte
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/TafveezA/EVM/evm"
+)
 
 func main() {
-	fmt.Println("Hello EVM")
+	contractdata := []byte{0x03, 0x0a, 0x02, 0x0a, 0x0b, 0x4f, 0x0c, 0x4f, 0x0c, 0x046, 0x0c, 0x0d, 0x03, 0x0a, 0x0f}
+	contractdata1 := []byte{}
+	contractdata = append(contractdata, contractdata1...)
+	contractState := evm.NewState()
+
+	vm := evm.NewVM(contractdata, contractState)
+	vm.Run()
+
+	fmt.Printf("%v\n", contractState)
 }
